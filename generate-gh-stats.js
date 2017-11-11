74bf0f6b7bc7856c21efefdbb5e7a6d3cdf4a404
Joshua Castor <jcasto3@uic.edu>
2017-11-11 17:12:46 -0600

    Added more code for project entry pre-processing

--- generate-gh-stats.js
+++ generate-gh-stats.js
@@ -41,7 +41,7 @@ query {
       totalCount
       nodes{
         name
-        
+        homepageUrl
         description
         shortDescriptionHTML
         pushedAt
@@ -94,9 +94,81 @@ query {
     }
   }
 }
-`
+`;
+
+function createProjectEntry(projectData, customData) {
+  let project = {};
+  project.repoName = projectData.name;
+  project.description = projectData.description;
+  project.homepageURL = projectData.homepageUrl;
+  project.lastPushedAt = projectData.lastPushedAt;
+  project.createdAt = projectData.createdAt;
+  project.repoURL = projectData.url;
+  project.owner = projectData.nameWithOwner.split("/")[0];
+
+  project.topics = [];
+  if (projectData.repositoryTopics.nodes.length > 0) {
+    for (let t of projectData.repositoryTopics.nodes) {
+      let topic = {
+        name: t.topic.name,
+        url: t.url
+      };
+      project.topics.push(topic);
+    }
+  }
+
+  project.languages = [];
+  if (projectData.languages.edges.length > 0) {
+    for (let lang of projectData.languages.edges) {
+      let languageEntry = {
+        name: lang.node.name,
+        color: lang.node.color,
+        size: lang.size
+      };
+      project.languages.push(languageEntry);
+    }
+  }
+
+  // TODO: implement tech used from custom-project-data.json
+  project.techUsed = [];
+
+  return project;
+}
 
 sendQuery(repoQuery).then((result) => {
-    fs.writeFileSync('gh-projects.json',JSON.stringify(result,null,2),'utf8');
-    console.log("Done");
+  console.log("Saving GH JSON object");
+  fs.writeFileSync('gh-projects.json',JSON.stringify(result,null,2),'utf8');
+  return result.data;
+}).then((ghData) => {
+  console.log("Creating project-data.json");
+  let customData = JSON.parse(fs.readFileSync("custom-project-data.json",'utf8'));
+
+  let finalProjectData = {};
+
+  let ghProjects = ghData.user.contributedRepositories.nodes;
+  for(let p of ghProjects){
+    if(!finalProjectData[p.nameWithOwner] && customData.ignoredProjects.indexOf(p.nameWithOwner) === -1){
+      console.log("Adding", p.nameWithOwner, "in contributedRepositories list");
+      finalProjectData[p.nameWithOwner] = createProjectEntry(p);
+    }else{
+      console.log("Skipping",p.nameWithOwner,"in contributedRepositories list");
+    }
+  }
+
+  ghProjects = ghData.user.repositories.nodes;
+  for (let p of ghProjects) {
+    if (!finalProjectData[p.nameWithOwner] && customData.ignoredProjects.indexOf(p.nameWithOwner) === -1) {
+      console.log("Adding", p.nameWithOwner, "in repositories list");
+      finalProjectData[p.nameWithOwner] = createProjectEntry(p);
+    } else {
+      console.log("Skipping", p.nameWithOwner, "in repositories list");
+    }
+  }
+
+  fs.writeFileSync('project-data.json',JSON.stringify(finalProjectData,null,2),'utf8');
+
+  return;
+
+}).then(() => {
+  console.log("Done");
 }).catch(console.error);
