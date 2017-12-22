58a817d9a4c01a2c39775996a6679ac583d6b617
Joshua Castor <jcasto3@uic.edu>
2017-12-22 15:17:00 -0600

    Moved navbar component code to pageController

--- js/apps/pageController.js
+++ js/apps/pageController.js
@@ -2,11 +2,14 @@
 function PageController(options = {}) {
     let self = {
         models: null,
-        app: null
+        app: null,
+        components: {}
     };
 
     init();
     function init() {
+        initComponents();
+
         self.models = options.models;
         self.views = options.views;
         self.log = options.log;
@@ -18,12 +21,69 @@ function PageController(options = {}) {
             },
             methods: {
                 setPageTo: setPageTo
-            }
+            },
+            components: self.components
         });
     }
 
+    function initComponents() {
+        self.components['jccc-nav-header'] = {
+            props: ['pages'],
+            template: `
+                <div class="mdc-toolbar">
+                    <div class="mdc-toolbar__row sm-plus">
+                        <div class="mdc-toolbar__section mdc-toolbar__section--shrink-to-fit mdc-toolbar__section--align-start">
+                            <h2 class="mdc-toolbar__title"><a href="/">JCCC</a></h2>
+                        </div>
+                        <div class="mdc-toolbar__section mdc-toolbar__section--align-end">
+                            <nav id="page-tab-bar" class="mdc-tab-bar" role="tablist">
+                                <a role="tab" v-for="(page,key) in pages" 
+                                    :class="{ 'mdc-tab':true, 'mdc-tab--active': page.isActive }"
+                                    @click="setPageTo(key)"    
+                                >
+                                    {{ key }}
+                                </a>
+                                <span class="mdc-tab-bar__indicator"></span>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="mdc-toolbar__row xs-only" id="mobile-header">
+                        <div class="mdc-toolbar__section">
+                            <h2 class="mdc-toolbar__title"><a href="/">JCCC</a></h2>
+                        </div>
+                    </div>
+                    <div class="mdc-toolbar__row xs-only">
+                        <div class="mdc-toolbar__section mdc-toolbar__section--align-start">
+                            <nav id="page-tab-bar-mobile" class="mdc-tab-bar" role="tablist">
+                                <a role="tab" v-for="(page,key) in pages" 
+                                    :class="{ 'mdc-tab':true, 'mdc-tab--active': page.isActive }"
+                                    @click="setPageTo(key)"    
+                                >
+                                    {{ key }}
+                                </a>
+                                <span class="mdc-tab-bar__indicator"></span>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                `,
+            methods: {
+                setPageTo: function (newPage) {
+                    // emit changes to parent
+                    this.$emit("pagechange", newPage);
+                }
+            },
+            mounted: function () {
+                self.log("Mounted navbar");
+                $(this.$el).find(".mdc-tab-bar").each(function () {
+                    new mdc.tabs.MDCTabBar(this);
+                });
+            }
+        };
+    }
+
     // change view of page
-    function setPageTo(pageName) {
+    function setPageTo(pageName, isExternal = false) {
         let pages = $(".pages");
         let delay = 100, fadePromises = [];
         for(let p in self.models.pages){
