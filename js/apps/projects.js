8cb25e75075f8b1e921f978f9e3dfa5197612d5c
Joshua Castor <jcasto3@uic.edu>
2018-01-05 13:21:50 -0600

    Added initial search card

--- js/apps/projects.js
+++ js/apps/projects.js
@@ -23,9 +23,79 @@ function ProjectsApp(options = {}) {
             methods: {},
             components: self.components
         });
+
+        self.log(self.models);
     }
 
     function initComponents() {
+        let nameSearch, languageBoxes = {}, nameButtons = {};
+        self.components["name-search"] = {
+            template: `
+                <div class="mdc-form-field name-search">
+                    <div class="mdc-text-field">
+                        <input type="text" id="name-field" class="mdc-text-field__input">
+                        <label class="mdc-text-field__label" for="name-field">Project Name</label>
+                        <div class="mdc-text-field__bottom-line"></div>
+                    </div>
+                </div>
+            `,
+            mounted: function(){
+                nameSearch = new mdc.textField.MDCTextField(this.$el);
+                self.log("[name-search]",nameSearch);
+            },
+            methods: {
+                input: function () {
+                    // debouncing?
+                }
+            }
+        };
+
+        self.components["language-checkbox"] = {
+            props: ['language'],
+            template: `
+                <div class="langauge-checkbox mdc-form-field">
+                    <div class="mdc-checkbox">
+                        <input type="checkbox" :id="language" class="mdc-checkbox__native-control"/>
+                        <div class="mdc-checkbox__background">
+                            <svg class="mdc-checkbox__checkmark"
+                                viewBox="0 0 24 24">
+                                <path class="mdc-checkbox__checkmark__path"
+                                    fill="none"
+                                    stroke="white"
+                                    d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
+                            </svg>
+                            <div class="mdc-checkbox__mixedmark"></div>
+                        </div>
+                    </div>
+
+                    <label :for="language">{{ language }}</label>
+                </div>
+            `,
+            created: function() {
+                // self.log(this);
+            }
+        };
+
+        self.components["radio-button"] = {
+            props: ['value', 'groupname', 'actualname'],
+            template: `
+                <div class="mdc-form-field">
+                    <div class="mdc-radio">
+                        <input class="mdc-radio__native-control" type="radio" :id="value" v-bind:name="groupname" :value="value">
+                        <div class="mdc-radio__background">
+                            <div class="mdc-radio__outer-circle"></div>
+                            <div class="mdc-radio__inner-circle"></div>
+                        </div>
+                    </div>
+                    <label :for="value">{{ actualname }}</label>
+                </div>
+            `,
+            mounted: function() {
+                nameButtons[`${this.$attrs.groupname}/${this.$attrs.actualname}`] = new mdc.radio.MDCRadio(this.$el);
+                
+                self.log("[radio-button]",this,nameButtons);
+            }
+        };
     }
 
     return {};
