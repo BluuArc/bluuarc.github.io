9eafce0b29b4e82c4cdfcb7df0271ee42f6d7267
Joshua Castor <jcasto3@uic.edu>
2018-05-06 19:33:03 -0500

    Removed helloworld component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
 import HomePage from '@/components/Home/Main';
 import ProjectsPage from '@/components/Projects/Main';
 import ContactPage from '@/components/Contact/Main';
@@ -9,11 +8,6 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/helloworld',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/',
       name: 'Home',
