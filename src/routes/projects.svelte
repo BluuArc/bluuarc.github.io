f4a28eec4a99ce7416716a2a8a89b71d42409945
BluuArc <joshua.castor@joshuacastor.me>
2021-04-16 19:14:33 -0500

    Added placeholder pages for posts and projects

---
+++ src/routes/projects.svelte
@@ -0,0 +1,10 @@
+<script context="module" lang="ts">
+	import { defaultLoadMethodForMethodStore } from '$lib/stores/fetchMethod';
+	export function load(args) {
+		return defaultLoadMethodForMethodStore(args);
+	}
+</script>
+
+<main>
+	Projects page TBD
+</main>
