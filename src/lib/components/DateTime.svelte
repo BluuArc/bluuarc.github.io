ced7787575ec01f93f9dc94a6191db02d80e09ff
BluuArc <joshua.castor@joshuacastor.me>
2021-04-25 16:51:05 -0500

    Add DateTime component for visually displaying relative date information

---
+++ src/lib/components/DateTime.svelte
@@ -0,0 +1,22 @@
+<script context="module" lang="ts">
+	import dayjs from 'dayjs';
+	import relativeTime from 'dayjs/plugin/relativeTime';
+	dayjs.extend(relativeTime);
+</script>
+
+<script lang="ts">
+	export let dateTime: string = (new Date()).toDateString();
+	let differenceFromNow: string;
+	let parsedDateString: string;
+	let isoDateString: string;
+
+	$: {
+		const parsedDate = new Date(dateTime);
+		differenceFromNow = dayjs(parsedDate).fromNow();
+		parsedDateString = parsedDate.toDateString();
+		isoDateString = parsedDate.toISOString();
+	}
+</script>
+
+<time datetime={isoDateString} title={parsedDateString}>{differenceFromNow}<span class="sr-only"> ({parsedDateString})</span></time>
+
